b1c579b716451a1d4a6961bf03103f7d54361010
justingruen <fanestudios0@gmail.coim>
2020-09-29 19:30:20 -0400

    Updates

---
+++ precache-manifest.6e180f5cc21b428e7d486a33cd7afcf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5fd430fe9cc2c1d6e4097e3b982f9ef",
+    "url": "/reciperepo/index.html"
+  },
+  {
+    "revision": "d48bde3a712a2f665424",
+    "url": "/reciperepo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "27ba519fca9bdf9ac84e",
+    "url": "/reciperepo/static/js/2.3dc064e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reciperepo/static/js/2.3dc064e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d48bde3a712a2f665424",
+    "url": "/reciperepo/static/js/main.8013164c.chunk.js"
+  },
+  {
+    "revision": "691c8438d45108507009",
+    "url": "/reciperepo/static/js/runtime-main.f32392ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reciperepo/static/media/logo.5d5d9eef.svg"
+  }
+]);
